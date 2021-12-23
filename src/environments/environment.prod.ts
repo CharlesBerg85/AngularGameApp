542354c1649b65c169c64c70afe6ca42ac0aa9ce
Charles M Berg <77517285+CharlesBerg85@users.noreply.github.com>
2021-12-22 23:57:10 -0800

    Connected firebase and imports work

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAWNSxwMhsVxp6-4OgD6xe33wesDZPco2k",
+    authDomain: "gamelist-98c1d.firebaseapp.com",
+    projectId: "gamelist-98c1d",
+    storageBucket: "gamelist-98c1d.appspot.com",
+    messagingSenderId: "971268960505",
+    appId: "1:971268960505:web:48fb34cb077ae114e2f228",
+    measurementId: "G-JWMYXF749J"
+}
 };
