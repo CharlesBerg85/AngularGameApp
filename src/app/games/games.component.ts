7998963019b1701af0a90bf2ac5f8ddcb0705b66
Charles M Berg <77517285+CharlesBerg85@users.noreply.github.com>
2022-01-05 22:13:50 -0800

    added game model and genre enum

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../models/game';
+import { Genre } from '../types/genre';
 
 @Component({
   selector: 'app-games',
@@ -6,8 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./games.component.css']
 })
 export class GamesComponent implements OnInit {
-  games = ['Resident Evil', 'Silent Hill', 'UnCharted']
-  constructor() { }
+  games: Game[] = [
+    {
+      id: 1,
+      name: 'Resident Evil',
+      genre: Genre.Horror 
+    },
+    {
+      id: 2,
+      name: 'UnCharted',
+      genre: Genre.Adventure 
+    },
+    {
+      id: 3,
+      name: 'Street Fighter',
+      genre: Genre.Fighting 
+    }
+  ]
+  constructor() { 
+
+  }
 
   ngOnInit(): void {
   }
---
+++ src/app/models/game.ts
@@ -0,0 +1,7 @@
+import { Genre } from "../types/genre";
+
+export class Game {
+    id: number | undefined;
+    name: string | undefined;
+    genre: Genre | undefined;
+}
