f9bb1a18499a8a9d348ac40cab8f3005d84b1825
Charles M Berg <77517285+CharlesBerg85@users.noreply.github.com>
2022-01-13 21:49:18 -0800

    file paths corrected and modules working

--- src/app/modules/games/games.module.ts
+++ src/app/modules/games/games.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { GamesComponent } from './components/games/games.component';
+import { GamesService } from './services/games.service';
 
 @NgModule({
-  declarations: [],
+  declarations: [GamesComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  providers: [GamesService],
+  exports: [GamesComponent]
 })
 export class GamesModule { }
